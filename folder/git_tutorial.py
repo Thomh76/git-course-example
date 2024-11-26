db94d4f1183f8f68316b7fcf1503315cf1f7fcaa
Tomasz Wójcik <tomek.e.wojcik@gmail.com>
2024-11-26 21:53:41 +0100

    final for now

--- folder/git_tutorial.py
+++ folder/git_tutorial.py
@@ -1,5 +1,8 @@
 """Learing git"""
 
+from dataclasses import dataclass
+from typing import NamedTuple, TypedDict
+
 
 def add_number(a: int, b: int, c: int) -> int:
     """Return the sum of a, b and c."""
@@ -10,3 +13,29 @@ def add_number(a: int, b: int, c: int) -> int:
 def calc_difference(a: int, b: int) -> int:
     """Return difference between a and b."""
     return a - b
+
+
+class Point(NamedTuple):
+    """Class to keep 2D points coordinates"""
+
+    x: int
+    y: int
+
+
+point_a = Point(1, 2)
+
+
+class Student(TypedDict):
+    name: str
+    age: int
+    position: Point
+    friends: list["Student"]
+
+
+student: Student = {"name": "Marcy", "age": 25, "position": Point(2, 3), "friends": []}
+
+
+@dataclass
+class point_b:
+    x: int
+    y: int
