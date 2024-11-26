bf90b5d2d435c777b3d43339475baee8c95a471a
Tomasz Wójcik <tomek.e.wojcik@gmail.com>
2024-11-26 19:49:31 +0100

    add types

--- folder/git_tutorial.py
+++ folder/git_tutorial.py
@@ -1,8 +1,12 @@
-'''Learing git'''
-def add_number(a, b, c):
-    '''Return the sum of a, b andc.'''
-    return sum(a, b, c)
+"""Learing git"""
 
-def calc_difference(a, b):
-    '''Return difference between a and b.'''
+
+def add_number(a: int, b: int, c: int) -> int:
+    """Return the sum of a, b and c."""
+    return sum([a, b, c])
+    # return a + b + c
+
+
+def calc_difference(a: int, b: int) -> int:
+    """Return difference between a and b."""
     return a - b
